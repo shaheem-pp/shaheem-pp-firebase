c8f100bb7aaf185ee9e79d92eb13bd16dadc5443
the-codeholic <shanofficial2000@gmail.com>
2022-05-29 11:01:18 +0530

    minor updates for reveal

--- public/Assets/JS/script.js
+++ public/Assets/JS/script.js
@@ -24,7 +24,7 @@ function reveal() {
 	for (var i = 0; i < reveals.length; i++) {
 		var windowHeight = window.innerHeight;
 		var elementTop = reveals[i].getBoundingClientRect().top;
-		var elementVisible = 150;
+		var elementVisible = 10;
 		if (elementTop < windowHeight - elementVisible) {
 			reveals[i].classList.add("active");
 		} else {
